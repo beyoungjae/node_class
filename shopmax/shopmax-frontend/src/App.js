55fce15872b7782c93c10a630f236999d4b16007
beyoungjae <pz1998@naver.com>
2025-01-08 16:39:14 +0900

    up

--- shopmax/shopmax-frontend/src/App.js
+++ shopmax/shopmax-frontend/src/App.js
@@ -1,22 +1,27 @@
 import './styles/common.css'
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
 import { checkAuthStatusThunk } from './features/authSlice'
+import { useEffect } from 'react'
 
 import Navbar from './components/shared/Navbar'
 import Footer from './components/shared/Footer'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import Home from './pages/Home'
+import ItemCreatePage from './pages/ItemCreatePage'
+import ItemEditPage from './pages/ItemEditPage'
+import ItemListPage from './pages/ItemListPage'
 
 import RedirectLoginRoute from './components/auth/RedirectLoginRoute'
+import AdminRoute from './components/auth/AdminRoute'
+import { Toolbar } from '@mui/material'
 
 function App() {
    const dispatch = useDispatch()
-   const { isAuthenticated, user } = useSelector((state) => state.auth) // 로그인 상태, 로그인 한 사용자 정보
+   const { isAuthenticated, user } = useSelector((state) => state.auth) //로그인 상태, 로그인 한 사용자 정보
 
-   // 새로고침 시 redux state가 초기화 되거나, 프로그램 실행 중 문제 발생 시 로그인이 초기화가 될 수 있으므로 지속적인 로그인 상태 확인을 위해 사용
+   //새로고침시 redux state가 초기화 되거나 프로그램 실행 중 문제 발생 가능성이 있으므로 지속적인 로그인 상태 확인을 위해 사용
    useEffect(() => {
       dispatch(checkAuthStatusThunk())
    }, [dispatch])
@@ -24,6 +29,8 @@ function App() {
    return (
       <>
          <Navbar isAuthenticated={isAuthenticated} user={user} />
+         {/* 메뉴에 아래 컨텐츠가 가려져서 보이는 문제 해결 -> Appbar 높이만큼 여백추가 */}
+         <Toolbar />
          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<SignupPage />} />
@@ -34,6 +41,37 @@ function App() {
                   <RedirectLoginRoute>
                      <LoginPage />
                   </RedirectLoginRoute>
+               }
+            />
+
+            {/* 상품 등록 */}
+            <Route
+               path="/items/create"
+               element={
+                  // 관리자가 아닐경우 home으로 리다이렉트
+                  <AdminRoute>
+                     <ItemCreatePage />
+                  </AdminRoute>
+               }
+            />
+            {/* 상품 수정 */}
+            <Route
+               path="/items/edit/:id"
+               element={
+                  // 관리자가 아닐경우 home으로 리다이렉트
+                  <AdminRoute>
+                     <ItemEditPage />
+                  </AdminRoute>
+               }
+            />
+            {/* 상품리스트 */}
+            <Route
+               path="/items/createlist"
+               element={
+                  // 관리자가 아닐경우 home으로 리다이렉트
+                  <AdminRoute>
+                     <ItemListPage />
+                  </AdminRoute>
                }
             />
          </Routes>
